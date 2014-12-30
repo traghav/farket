956cb369a485ce133e87215a781c6ae160f13bd3
Raghav <raghav.toshniwal@gmail.com>
2014-12-31 01:54:14 +0530

    Insert Functions working

--- controllers/default.py
+++ controllers/default.py
@@ -8,7 +8,7 @@
 ## - download is for downloading files uploaded in the db (does streaming)
 ## - api is an example of Hypermedia API support and access control
 #########################################################################
-
+import datetime
 def index():
     """
     example action using the internationalization operator T and flash
@@ -19,18 +19,71 @@ def index():
     """
     response.flash = T("Welcome to web2py!")
     return dict(message=T('Hello World'))
+
+
 def test():
     return "some stuff"
-def displayDB():
-    tlist=[]
+def displayCrop():
+    clist=[]
     for row in db().select(db.crop.ALL):
         x = row.cropName + " "
-        tlist.append(x)
-    return dict(tlist)
+        clist.append(x)
+    return dict(message=clist)
+   
+def displayBuyer():
+    Blist=[[],[]]
+    count=0
+    for row in db().select(db.Buyer.ALL):
+        x = row.email + " "
+        y = row.name + " " 
+        Blist[len(Blist):].append(x)
+        Blist[len(Blist):].append(y)
+        count=count+1
+    return dict(message=Blist)
+@service.run
+def fSellerID(num):
+    insertSeller(num)
+    for row in db(db.Seller.phonenumber==num).select():
+        return row.id
+@service.run
+def fCropID(crName):
+    for row in db(db.crop.cropName==crName).select():
+        return row.id
+@service.run
+def insertSupply(num,p,cr):
+    dt = datetime.date.today()
+    loca="India"
+    sid=fSellerID(num)
+    cid=fCropID(cr)
+    db.SupplyList.insert(Price=p,tstamp=dt,loc=loca,seller_id=sid,crop_id=cid)
+    return "yay"
+def displaySlist():
+    return dict(message=db().select(db.SupplyList.ALL))
+
+    
+
+
+
 
 def insertCrop():
     db.crop.insert(cropName='Wheat')
     print "success"
+@service.run    
+def insertBuyer(e,n):
+    db.Buyer.insert(email=e,name=n)
+    return "inserted email "+e+" and name"
+@service.run
+def insertSeller(phone):
+    db.Seller.update_or_insert(phonenumber=phone)
+
+    
+def tryone():
+    return 'sample'
+
+
+
+
+
 
 def user():
     """
@@ -58,7 +111,10 @@ def download():
     """
     return response.download(request, db)
 
-
+# def concat(a,b):
+#     db.crop.insert(cropName=a)
+#     print "abra"
+#     return a+b
 def call():
     """
     exposes services. for example:
@@ -67,9 +123,13 @@ def call():
     supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
     """
     return service()
+@service.run
+def concat(a,b):
+    db.crop.insert(cropName=a)
+    print "abra"
+    return a+b
 
-
-@auth.requires_login() 
+"""@auth.requires_login() """
 def api():
     """
     this is example of API with access control
@@ -80,3 +140,10 @@ def api():
         '<tablename>': {'GET':{},'POST':{},'PUT':{},'DELETE':{}},
         }
     return Collection(db).process(request,response,rules)
+
+
+
+
+
+
+
