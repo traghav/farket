bdeb6921f4e735a4b8daccba19d74300221c7140
Raghav Toshniwal <raghav.toshniwal@gmail.com>
2015-01-31 01:14:58 +0530

    Database Fresh

--- controllers/default.py
+++ controllers/default.py
@@ -21,6 +21,8 @@ def index():
     """
     response.flash = T("Welcome to web2py!")
     return dict(message=T('Hello World'))
+def purge():
+    db.crop.truncate()
 
 def prepareMail():
     rows = db((db.SupplyList.crop_id==db.DemandList.crop_id) & (db.SupplyList.seller_id==db.Seller.id) & (db.DemandList.buyer_id==db.Buyer.id) &(db.SupplyList.crop_id==db.crop.id) & (db.SupplyList.Price<=db.DemandList.MaxPrice)).select(orderby=db.Buyer.id)
